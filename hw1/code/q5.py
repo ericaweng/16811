9abbd2fb24f7a60c33765ea1e5ff6a9a1e527673
EricaWeng <ericaxw99@gmail.com>
2020-09-22 08:05:22 -0400

    q5 completion

--- hw1/code/q5.py
+++ hw1/code/q5.py
@@ -7,59 +7,46 @@ from q3 import solve
 
 
 def problem5(points_in, points_out):
-  I = np.array(points_in)
-  O = np.array(points_out)
-  # A = O - I 
-  # U, S, Vh = svd(A)
-  
-  centerI = np.mean(I, axis=0)
-  centerO = np.mean(O, axis=0)
+  P = np.array(points_in)
+  Q = np.array(points_out)
 
-  translation = centerO - centerI
-  newO = O - centerO
-  newI = I - centerI
+  p_bar = np.mean(P, axis=1, keepdims=True)
+  q_bar = np.mean(Q, axis=1, keepdims=True)
 
-  # print_mats([newO, centerO, newI, centerI])
+  P_new = P - p_bar
+  Q_new = Q - q_bar
 
-  # U_i, S_i, Vh_i = svd(newI)
-  # U_o, S_o, Vh_o = svd(newO)
-  # print_mats([U_i, S_i, Vh_i])
-  # print_mats([U_o, S_o, Vh_o])
-  # A = newI.dot()
+  U, _, Vh = np.linalg.svd(P_new.dot(Q_new.T))
 
-  # rotation = I^{-1} O = V \frac{1}{\Sigma} U^T O
-  # rotation = solve(newI, newO)
-  # print(rotation)
+  R = Vh.T.dot(U.T)
+  t = q_bar - R.dot(p_bar) 
 
-  U, s, Vh = np.linalg.svd(newI)
-  sigma = s[s > 1e-3]
-  k = sigma.shape[0]
-  s[:k] = 1/sigma
-  D = sla.diagsvd(s, U.shape[0], Vh.shape[0])
-
-  P_1 = Vh.T.dot(D.T).dot(U.T)
-  rotation = newO.dot(P_1)
-  print("rotation matrix:\n", rotation)
-  translation = O - rotation.dot(I) 
-
-  # check
-  # print(rotation.dot(I + centerI) + centerO)
-  # print(rotation.dot(I) + translation)
-  print(rotation.T.dot(rotation)) #np.eye(rotation.shape[0])
-  assert close_enough(rotation.dot(I) + translation, O), "test case failed"
-  # assert np.all(np.multiply(I, rotation) + translation == O)
+  return R, t
 
 def main(): 
-  # a = [[1,2,3],[1,2,4],[1,4,2]]
-  # b = [[1,-2,1],[4,-3,3],[1,-3,4]]
-  PQs = [([[-12, 2, 0], [-8,9,0],[-4,4,0]], [[-4,0,0],[4,2,0],[0,-5,0]]),
-  ([[1,5,1],[4,1,9],[2,-1,-1],[6,3,-10]],
-    [[-1.633974596,  4.12252235,  2.708308788],[7.294228634, 6.622780076, 0.9659258263],[-0.3660254038 , 0.4482877361 , -2.380139389],[-10.16025404 ,2.544224088, -5.941057286]]),
-  ]
-  for i, (p, q) in enumerate(PQs):
-    print("\n"+"#"*12+"\ntest case %d\n" % i + "#"*12)
-    problem5(p, q)
+  # Generate random points
+  num_test_cases = 10
+  num_points = 20
+  num_dims = 3
 
+  P = np.random.uniform(0, 2, (num_dims, num_points))
+  Ps = np.stack([P for i in range(num_test_cases)])
+
+  # Generate random Rs and ts
+  Rs = np.array([sla.orth(mat) for mat in np.random.rand(num_test_cases, num_dims, num_dims)])
+  # if round(linalg.det(R)) == -1:
+  #     R[3] *= -1
+  ts = np.random.rand(num_test_cases, num_dims, 1)
+
+  # Generate Q from R and t
+  Qs = np.matmul(Rs, P) + ts
+
+  # test our function
+  for i, (p, q) in enumerate(zip(Ps, Qs)):
+    print("\n"+"#"*12+"\ntest case %d\n" % i + "#"*12)
+    R, t = problem5(p, q)
+    assert close_enough(Rs[i], R) and close_enough(ts[i], t), "failed. Rs: {} vs {}\t ts: {} vs. {}".format(Rs[i], R, ts[i], t)
+    print("R:{}\nt:{}\npassed".format(R, t))
 
 if __name__ == '__main__':
   main()
