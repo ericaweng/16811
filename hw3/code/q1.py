fe7bc30c995da9e23b37e45dc7a3fed491e8126d
EricaWeng <ericaxw99@gmail.com>
2020-10-14 21:46:00 -0400

    q4e and q1 edits

--- hw3/code/q1.py
+++ hw3/code/q1.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from torch import nn
 import torch.optim as optim
+import torch.nn.functional as F
 
 
 def quad(a, b, c):
@@ -16,18 +17,19 @@ def quad_p(a, b, c):
 	return f_p
 
 def main():
-	a = nn.Parameter(torch.FloatTensor(np.random.randn(1)))
-	b = nn.Parameter(torch.FloatTensor(np.random.randn(1)))
-	c = nn.Parameter(torch.FloatTensor(np.random.randn(1)))
+	# np.random.randn(1)
+	a = nn.Parameter(torch.FloatTensor([0.001]))
+	b = nn.Parameter(torch.FloatTensor([.774]))
+	c = nn.Parameter(torch.FloatTensor([-5]))
 
 	f = lambda x: np.sin(x) - 0.5
 	p = quad(a, b, c)
 
-	optimizer = optim.SGD([a,b,c], lr=.1)
+	optimizer = optim.SGD([a,b,c], lr=.008)
 
 	x = torch.FloatTensor(np.arange(-np.pi/2, np.pi/2, .000001))
 
-	for _ in range(1000):
+	for _ in range(1200):
 		loss = torch.mean((f(x) - p(x))**2)
 		# loss = torch.max(torch.abs((f(x) - p(x))))
 		print(loss.data.numpy().item())
@@ -36,9 +38,10 @@ def main():
 		loss.backward()
 		optimizer.step()
 
-	print("p(x) = {0.4f}x^2 + {}x + {}".format(a.data.numpy().item(), b.data.numpy().item(),c.data.numpy().item()))
-	print("$L2$-error:",  torch.mean((f(x) - p(x))**2))
-	print("$L-\\infty$-error:",  torch.max(torch.abs(f(x) - p(x))))
+	# torch.mean((f(x) - p(x))**2)
+	print("p(x) = {:0.9f}x^2 + {:0.4f}x + {:0.4f}".format(a.data.numpy().item(), b.data.numpy().item(),c.data.numpy().item()))
+	print("$L2$-error:", F.mse_loss(f(x), p(x)).data.numpy().item())
+	print("$L-\\infty$-error:", torch.max(torch.abs(f(x) - p(x))).data.numpy().item())
 
 if __name__ == '__main__':
 	main()
--- hw3/code/q4.py
+++ hw3/code/q4.py
@@ -9,7 +9,11 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
+def get_plane_eq(best_fit):
+    return "{:0.3f}x + {:0.3f}y - z = {:0.3f}".format(*best_fit)
+
 def fit(Xy):
+    '''fit a plane to a set of points'''
     xs = Xy[:, :2]
     zs = Xy[:, 2]
     reg = LinearRegression().fit(xs, zs)
@@ -18,21 +22,62 @@ def fit(Xy):
 
     return a, b, c
 
+
 def get_distance(points, a, b, c):
+    '''vectorized distance formula for a set of points and a plane'''
     return np.abs(points.dot(np.array([a, b, -1, c]))) / np.sqrt(a*a + b*b + 1)
 
+
+def q4c():
+    # with open("clear_table.txt") as f:
+    with open("cluttered_table.txt") as f:
+        Xy = np.array(list(map(lambda x: list(map(float, x.split())), f.read().splitlines())))
+
+    # initial fitting of points to a plane
+    a, b, c = fit(Xy)
+
+    # part 4c
+    distance_thresh = .12
+    for i in range(50):
+        # concat additional constant to each point for distance calculation
+        points = np.concatenate((Xy, np.ones((Xy.shape[0], 1))), axis=-1)
+        distance = np.abs(points.dot(np.array([a, b, -1, c]))) / np.sqrt(a*a + b*b + 1)
+        mean_distance = np.mean(distance)
+        print("mean_distance:", mean_distance)
+        Xyi = Xy[distance < distance_thresh]
+        print("number of samples left:", Xyi.shape)
+        a, b, c = fit(Xyi)
+        distance_thresh *= .9
+
+    # print plane equation
+    print("{:0.3f}x + {:0.3f}y - z = {:0.3f}".format(a, b, -c))
+
+    # make data for plotting
+    X, Y = np.meshgrid(Xy[:, 0], Xy[:, 1])
+    Z = a * X + b * Y  + c
+
+    # plot
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    scatter = ax.scatter(Xyi[:, 0], Xyi[:, 1], Xyi[:, 2])
+    plt.show()
+
+
 def q4d():
+    # with open("clean_hallway.txt") as f:
     with open("cluttered_hallway.txt") as f:
         Xy = np.array(list(map(lambda x: list(map(float, x.split())), f.read().splitlines())))
         Xy = np.concatenate((Xy, np.ones((Xy.shape[0], 1))), axis=-1)
 
     num_points = Xy.shape[0]
-    k = 10
-    num_planes = 4
-    num_iters = 8000
-    distance_thresh = .12
-    distance_thresh_elim = .03
-    inlier_num_thresh = int(num_points / 5.9)
+
+    # hyperparameters
+    k = 5  # number of points to randomly sample each iteration
+    num_planes = 4  # number of planes to find
+    num_iters = 5000  # number of iterations to do for each plane
+    distance_thresh = .12  # minimum distance to be considered an inlier for a plane (to be considered for the top best plane)
+    distance_thresh_elim = .03  # distance threshold; points underneath will be eliminated in looking for the next plane
+    inlier_num_thresh = int(num_points / 6)  # minimum points within distance_thresh needed for a plane to be considered for top best plane
     print("inlier_num_thresh:", inlier_num_thresh)
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
@@ -76,7 +121,7 @@ def q4d():
         print("inlier_num_thresh:", inlier_num_thresh)
 
         # print plane equation
-        plane = "{:0.3f}x + {:0.3f}y - z = {:0.3f}".format(*best_fit)
+        plane = get_plane_eq(best_fit)
         print(plane)
         print(best_err)
 
@@ -87,6 +132,7 @@ def q4d():
         X, Y = np.meshgrid(Xy_plane[:, 0], Xy_plane[:, 1])
         Z = a * X + b * Y  + c
 
+        # get rid of accounted-for points
         Xy = Xy[distance_save > distance_thresh_elim]
         print("number of samples left:", Xy.shape)
 
@@ -97,44 +143,13 @@ def q4d():
 
     # scatter = ax.scatter(Xy[:, 0], Xy[:, 1], Xy[:, 2])
     # plt.savefig("10.png")
+
     np.savez("planes.npz", planes)
     print("\n".join(["{:0.3f}x + {:0.3f}y - z = {:0.3f}".format(*best_fit) for best_fit, _ in planes]))
 
 
-def q4c():
-    # with open("clear_table.txt") as f:
-    with open("cluttered_table.txt") as f:
-        Xy = np.array(list(map(lambda x: list(map(float, x.split())), f.read().splitlines())))
-
-    a, b, c = fit(Xy)
-
-    # part 4c
-    distance_thresh = .12
-    for i in range(50):
-        points = np.concatenate((Xy, np.ones((Xy.shape[0], 1))), axis=-1)
-        distance = np.abs(points.dot(np.array([a, b, -1, c]))) / np.sqrt(a*a + b*b + 1)
-        mean_distance = np.mean(distance)
-        print("mean_distance:", mean_distance)
-        Xyi = Xy[distance < distance_thresh]
-        print("number of samples left:", Xyi.shape)
-        a, b, c = fit(Xyi)
-        distance_thresh *= .9
-
-    # print plane equation
-    print("{:0.3f}x + {:0.3f}y - z = {:0.3f}".format(a, b, -c))
-
-    # make data for plotting
-    X, Y = np.meshgrid(Xy[:, 0], Xy[:, 1])
-    Z = a * X + b * Y  + c
-
-    # plot
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-    scatter = ax.scatter(Xyi[:, 0], Xyi[:, 1], Xyi[:, 2])
-    plt.show()
-
-
 def viz_planes():
+    # with open("clean_hallway.txt") as f:
     with open("cluttered_hallway.txt") as f:
         Xy1 = np.array(list(map(lambda x: list(map(float, x.split())), f.read().splitlines())))
 
@@ -142,17 +157,33 @@ def viz_planes():
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-    # print(planes.get('arr_0'))
-    for plane, Xy in planes.get('arr_0'):
+    num_best = 4
+    best_planes = sorted(planes.get('arr_0'), key=lambda x: x[1].shape[0])[-num_best:]
+    
+    mesh_thinner = 20
+
+    # sorted_smoothness = sorted([(plane, np.mean(np.square(get_distance(Xy, *plane))), Xy) 
+    #     for plane, Xy in best_planes], key=lambda x: x[1])
+
+    # for plane, _, Xy in sorted_smoothness[0:1]:
+    for plane, Xy in best_planes:
         a, b, c_neg = plane
-        X, Y = np.meshgrid(Xy[::20, 0], Xy[::20, 1])
+        X, Y = np.meshgrid(Xy[::mesh_thinner, 0], Xy[::mesh_thinner, 1])
         Z = a * X + b * Y - c_neg
         surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
     scatter = ax.scatter(Xy1[:, 0], Xy1[:, 1], Xy1[:, 2])
     plt.show()
 
+    sorted_smoothness = sorted([(plane, np.mean(np.square(get_distance(Xy, *plane)))) 
+        for plane, Xy in best_planes], key=lambda x: x[1])
+    print("\n".join(["{}\t{}".format(get_plane_eq(plane), loss) for plane, loss in sorted_smoothness]))
+
+
+def q4e():
+    pass
+
 
 if __name__ == '__main__':
-    # q4d()  
+    # q4d()
     viz_planes()
