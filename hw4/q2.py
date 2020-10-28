ced8bc9f43e295a94e2a409d2ccdea0bc0014b79
EricaWeng <ericaxw99@gmail.com>
2020-10-28 08:51:24 -0400

    test q5

--- hw4/q2.py
+++ hw4/q2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D # <--- This is important for 3d plotting 
 
 
 def main():
@@ -12,12 +13,38 @@ def main():
 	dZx = 3*X**2 + Y**3 - 4*X  + 3*Y **2
 	dZy = X**3+ 3*Y**2 - 2* X**2 + 6*Y 
 	fig, ax = plt.subplots()
-	CS = ax.contour(X, Y, dZx, 0)
-	CS = ax.contour(X, Y, dZy, 0)
-	ax.axis("equal")
-	# ax.clabel(CS, inline=1, fontsize=10)
-	# ax.set_title('Simplest default with labels')
+	# plt.imshow(Z)
+	contour_zs = np.arange(-2, 5, .5)
+	# CS = ax.contour(X, Y, dZx, contour_zs)
+	# CS = ax.contour(X, Y, dZy, contour_zs)
+	# CS = ax.contour(X, Y, dZx, 0, colors='red')
+	# CS = ax.contour(X, Y, dZy, 0, colors='red')
+	# CS = ax.contour(X, Y, Z)
+	fig3d = plt.figure()
+	ax3d = fig3d.add_subplot(111, projection='3d')
+	ax3d.plot_surface(X, Y, Z, cmap=plt.get_cmap('coolwarm'))
+
+	# CS = ax3d.plot3D(X, Y, dZx, color='red')
+	CS = ax3d.contour(X, Y, dZx, [0], colors='red')
+	CS = ax3d.contour(X, Y, dZy, [0], colors='red')
+	plt.tight_layout()
 	plt.show()
+
+
+	1, -1
+	# ax.axis("equal")
+	# plt.show()
+
+def plot2d(path_init, i=None, q_num=None, show=False):
+	# plt.plot(path_init[:, 0], path_init[:, 1], 'ro')
+	plt.tight_layout()
+	if show:
+		plt.show()
+	else:
+		plt.savefig("{}-{}".format(q_num, i))
+	plt.clf()
+
+
 
 if __name__ == '__main__':
 	main()
